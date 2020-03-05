eb5544c8b9295e056b907c09a6d5b1c94362d4f3
mohamed hani <mohamedhani01014@gmail.com>
2020-03-05 12:39:42 -0500

    second commit

--- main/lb/global_function.sh
+++ main/lb/global_function.sh
@@ -9,4 +9,23 @@
    fi
    return $OUTPUT
 }
+function number_check 
+{
+     RE="^[0-9]+$"
+     OUTPUT=0
+     if  [ -z $1 ]
+     then
+          echo "you dont enter any value" 
+     elif  [[ ! $1 =~ $RE ]]
+     then
+         echo "value you entered is not a number"
+     elif [[ $1 -gt $3 || $1 -lt $2 ]]
+     then
+         echo "enter value between $2 and $3"
+     else
+        OUTPUT=1
+     fi
+     return $OUTPUT
+
+}
 
